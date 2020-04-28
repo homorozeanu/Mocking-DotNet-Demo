a61a735c4b2a242b7ea5c149505a0259998d9736
vrpf6e <george.homorozeanu@siemens.com>
2020-04-28 17:53:44 +0200

    Add tests with FakeItEasy

---
+++ Model.Test/FakeItEasy/CalculationTests.cs
@@ -0,0 +1,37 @@
+﻿using FakeItEasy;
+using NUnit.Framework;
+
+namespace Model.Tests.FakeItEasy
+{
+    [TestFixture]
+    public class CalculationTests
+    {
+        [Test]
+        public void GetName_ShouldGetValueFromBusinessContext()
+        {
+            var businessContextMock = A.Fake<IBusinessContext>();
+            var calculation = new Calculation(businessContextMock);
+            string expectedName = "foo";
+            A.CallTo(() => businessContextMock.GetTransalation(calculation, nameof(Calculation.Name)))
+                .Returns(expectedName);
+
+            var actualName = calculation.Name;
+
+            A.CallTo(() => businessContextMock.GetTransalation(calculation, nameof(Calculation.Name)))
+                .MustHaveHappened();
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [Test]
+        public void SetName_ShouldPassValueToBusinessContext()
+        {
+            var businessContextMock = A.Fake<IBusinessContext>();
+            var calculation = new Calculation(businessContextMock);
+
+            var expectedName = "foo";
+            calculation.Name = expectedName;
+
+            A.CallTo(()=>businessContextMock.SetTranslation(calculation, nameof(Calculation.Name), expectedName))
+                .MustHaveHappened();
+        }
+    }}
